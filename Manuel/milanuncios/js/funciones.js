7f5c3c78a43d1a7b1107ba582274d82bb882b4f5
Your Name <you@example.com>
2023-01-18 09:45:09 +0100

    Corregido errores en ejercicio MilAnuncios

--- Manuel/milanuncios/js/funciones.js
+++ Manuel/milanuncios/js/funciones.js
@@ -1,6 +1,9 @@
 function unomas(){
+    // En la variable contador guardo el valor del input con id numero. Y lo convierto a número -> parseInt
     var contador = parseInt($("input#numero").val());
+    // Se le suma uno al contador
     contador += 1;
+    // Se le cambia el valor del contador al input con id número
     $("input#numero").val(contador);
     //var cajetin = "<input type='file' name='archivo"+contador+"'>";
     let cajetin = `<input type="file" name="archivo${contador}">`;
