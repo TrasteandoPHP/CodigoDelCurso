71265570d44b46c89a948a6c7cd70f8f13c90512
Your Name <you@example.com>
2023-01-20 14:30:59 +0100

    Añadido Ejercicio Academia 20.01.2023

---
+++ Manuel/academia/grabaAlumnos.php
@@ -0,0 +1,33 @@
+<?php
+    $nombre = $_POST["nombre"];
+    $apellidos = $_POST["apellidos"];
+    $dni = $_POST["dni"];
+    $encryptedPass = base64_encode($_POST["pass"]); 
+    $email = $_POST["email"];
+    $codigoCurso = $_POST["cod_cur"];    
+
+    include ("./assets/php/conexion.php");
+
+    $sqlExisteAlumno = "SELECT * FROM alumnos WHERE dni_alu='$dni'";
+    $ejecutarSqlExisteAlumno = $conexion->query($sqlExisteAlumno);    
+
+    if(!$ejecutarSqlExisteAlumno->fetch_array()){
+        $sqlRegistroAlumno = "INSERT INTO alumno (nom_alu, ape_alu, dni_alu, pas_alu, dir_alu, email_alu) VALUES ('$nombre','$apellidos','$dni','$encryptedPass','$email','$codigoCurso')";
+        $ejecutarRegistroAlumno = $conexion->query($sqlRegistroAlumno);
+
+        if($ejecutarRegistroAlumno)
+        {
+            echo "Alumno registrado correctamente";
+        }
+            else
+        {
+            echo "Ha ocurrido un error durante el registro de los datos";
+        }
+    }
+    else 
+    {
+        echo "Este alumno ya está registrado";
+    }
+
+    echo "<br><br><button onclick='window.location.href=`./registroAlumnos.php`'>Volver</button>";
+?>    
