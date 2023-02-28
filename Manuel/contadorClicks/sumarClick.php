e1bf3c2600984db37e4e94269d810185e7581321
manuel casado <you@example.com>
2023-02-28 18:37:17 +0100

    Corregido error sumador contador

--- Manuel/contadorClicks/sumarClick.php
+++ Manuel/contadorClicks/sumarClick.php
@@ -3,16 +3,17 @@
     //echo "Pulsado boton Nº$botonPulsado";
 
     $conexion = new mysqli ("localhost","root","","contadorClicks");
-    $sqlConsultaContadorBoton = "SELECT * FROM  botonPulsado WHERE num_bot='$botonPulsado' ORDER BY pul_bot ASC";
+    $sqlConsultaContadorBoton = "SELECT * FROM  botonpulsado WHERE num_bot='$botonPulsado' ORDER BY pul_bot ASC";
     $ejecutarSqlConsultaContadorBoton = $conexion->query($sqlConsultaContadorBoton); 
     if($registroContadorBoton = $ejecutarSqlConsultaContadorBoton->fetch_array()){
-        $cantidadPulsada = $registroContadorBoton["pul_bot"];
-        $sqlActualizarContadorBoton = "UPDATE botonPulsado SET pul_bot='$cantidadPulsada+1' WHERE num_bot='$botonPulsado'"; 
+        $cantidadPulsaciones = $registroContadorBoton["pul_bot"];
+        $nuevaCantidadPulsaciones = $cantidadPulsaciones + 1;
+        $sqlActualizarContadorBoton = "UPDATE botonpulsado SET pul_bot='$nuevaCantidadPulsaciones' WHERE num_bot='$botonPulsado'";
+        $ejecutarSqlActualizarContadorBoton = $conexion->query($sqlActualizarContadorBoton); 
+        echo "Actualizado contador del boton nº$botonPulsado con $nuevaCantidadPulsaciones pulsaciones";
     } else {
-        $sqlGrabarContadorBoton = "INSERT INTO botonPulsado(num_bot, pul_bot) VALUES ('$botonPulsado','1')";
+        $sqlGrabarContadorBoton = "INSERT INTO botonpulsado(num_bot, pul_bot) VALUES ('$botonPulsado','1')";
         $ejecutarSqlGrabarContadorBoton = $conexion->query($sqlGrabarContadorBoton);
+        echo "Grabado boton nº$botonPulsado con 1ª pulsación";
     }
-    
-    echo "Actualizado contador del boton nº $botonPulsado";
-
 ?>
