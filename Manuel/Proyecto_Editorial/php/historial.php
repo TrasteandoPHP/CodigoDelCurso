53cd565d7d3d83c24981396b8bb45a2fa97d394a
Your Name <you@example.com>
2023-04-11 14:31:23 +0200

    Añadidos los cambios del Proyecto_Editorial del 11.04.2023

--- Manuel/Proyecto_Editorial/php/historial.php
+++ Manuel/Proyecto_Editorial/php/historial.php
@@ -3,36 +3,45 @@
     $usuario = $_POST["nombre"];
             
     $conexion = new mysqli("10.10.10.114","daniel","1234","biblioteca2");
-                $sqlConsultaLibrosLeidos = "SELECT * FROM reservas WHERE cod_usu='$usuario'";
-                $ejecutarSqlConsultaLibrosLeidos = $conexion->query($sqlConsultaLibrosLeidos);
-                if($ejecutarSqlConsultaLibrosLeidos->fetch_array()){
-                    foreach ($ejecutarSqlConsultaLibrosLeidos as $registro) {
-                        $codigoLibro = $registro["cod_lib"];
-                        $codigoUsuario = $registro["cod_usu"];
-                        $fechaInicial = $registro["finicial"];
-                        $fechaEntrega = $registro["fentrega"];
+    //$conexion = new mysqli("10.10.10.199","busters","1234","biblioteca");
 
-                        echo "
-                            <article>
-                                <a href='#' class='image'><img src='images/Libro_01.jpg' alt=''/></a>
-                                <h3>Título</h3>
-                                <p>$codigoLibro</p>
-                                <p>$codigoUsuario</p>
-                                <p>$fechaInicial</p>
-                                <p>$fechaEntrega</p>
-                                <ul class='actions'>
-                                    <li><a href='#' class='button'>Ver</a></li>
-                                </ul>
-                            </article>                 
-                        ";                        
-                    }
+    $sqlConsultaLibrosLeidos = "SELECT * FROM prestamos INNER JOIN libros USING(cod_lib) WHERE cod_usu='$usuario'";
+    //$sqlConsultaLibrosLeidos = "SELECT * FROM prestamos INNER JOIN libros USING(cod_lib) WHERE cod_usu='$usuario'";
+    $ejecutarSqlConsultaLibrosLeidos = $conexion->query($sqlConsultaLibrosLeidos);
+    if($ejecutarSqlConsultaLibrosLeidos->fetch_array()){
+        foreach ($ejecutarSqlConsultaLibrosLeidos as $registro) {
+            $nombreImagen = $registro["imagen_lib"];
+            $ruta = "./images/".$nombreImagen;
+            $nombreLibro = $registro["titulo_lib"];
+            $codigoUsuario = $registro["cod_usu"];
+
+            $fechaInicial = $registro["fentrega_pres"];
+            $fechaInicial = explode("-",$fechaInicial);                        
+            $fechaPrestamo = $fechaInicial[2]."-".$fechaInicial[1]."-".$fechaInicial[0];                
+
+            $fechaEntrega = $registro["fprevista_pres"];
+            $fechaEntrega = explode("-",$fechaEntrega);                        
+            $fechaDevolucion = $fechaEntrega[2]."-".$fechaEntrega[1]."-".$fechaEntrega[0];
+            
+            echo "                
+                <article>
+                    <a href='#' class='image'><img src='$ruta' alt='' /></a>
+                    <div style='height:60px; width:80%; text-align:center; overflow: hidden;'>
+                        <h4 style='font-family:cursive'>$nombreLibro</h4>
+                    </div>
+                    <p style='margin:0; font-family:auto;'>Fecha Préstamo</p>
+                    <p style='margin:0; font-family:auto; font-size:auto;'>$fechaPrestamo</p>
+                    <p style='margin:0; font-family:auto;'>Fecha Devolución</p>
+                    <p style='margin:0; font-family:auto; font-size:auto; color:red'>$fechaDevolucion</p>                                
+                </article>                  
+            ";                      
+        }
                           
-                } else {
-
-                    echo "
-                        <script>
-                            alert ('No se encontraron registros.');                            
-                        </script>
-                    ";                     
-                }                 
+    } else {
+        echo "
+            <article>
+                <h2>Sin historial. Parece que no lees mucho....</h2>
+            </article>    
+        ";                     
+    }                 
 ?>
