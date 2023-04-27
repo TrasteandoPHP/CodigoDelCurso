d5ad29e3f01de2c78c03367deea1ac120d7e1d44
manuel casado <you@example.com>
2023-04-27 21:53:26 +0200

    Añadido proyecto Bookbusters copia funcional en local

---
+++ Manuel/Proyecto_BookBusters_local/privado/funcionEncryptDecrypt.php
@@ -0,0 +1,48 @@
+<?php
+    // función que encripta y desencripta
+
+    function encriptado($accion, $texto){
+        $modo = "AES-128-ECB";
+        $llave = "mecachisenlamar";
+
+        if($accion == "e"){
+            $textoEncriptado = openssl_encrypt($texto, $modo, $llave);
+            return $textoEncriptado;
+            //return base64_encode($textoEncriptado);
+        } else {
+            //$texto = base64_decode($texto);
+            $textoDesencriptado = openssl_decrypt($texto, $modo, $llave);
+            return $textoDesencriptado;
+        }
+    }
+
+
+    // Llamadas a función y demás código
+
+    $email = "alfonso@medellin.ef";
+    echo "email texto plano: $email<br>";
+
+    $emailEncriptado = encriptado("e", $email);
+    echo "email encriptado: $emailEncriptado<br>";
+
+    // Desencriptando el correo.
+    $emailDesencriptado = encriptado("d", $emailEncriptado);
+
+    if($emailDesencriptado==$email){
+        echo "----------------------------------------------------------";
+        echo "";
+        echo "<br>Desencriptando email ........<b>email correcto</b><br>";
+        echo "";
+        echo "----------------------------------------------------------";
+    } else {
+        echo "------------------------------------------------------------";
+        echo "";
+        echo "<br>Desencriptando email ........<b>email incorrecto</b><br>";
+        echo "";
+        echo "------------------------------------------------------------";
+    }
+
+    
+
+    
+?>
---
+++ Manuel/Proyecto_BookBusters_local/privado/php/funciones.php
@@ -0,0 +1,91 @@
+<?php
+function encriptado($accion,$texto){
+    $modo = "AES-128-ECB";
+    $llave = "peche";
+    
+    if($accion=="e")
+    {
+    $textoencriptado= openssl_encrypt($texto,$modo,$llave);
+    $textoencriptado=base64_encode($textoencriptado);
+    return $textoencriptado;
+    }
+    else{
+        $textoencriptado=base64_decode($texto);
+        $textoencriptado= openssl_decrypt($textoencriptado,$modo,$llave);
+        return $textoencriptado;
+    }
+    }
+
+
+function recoge(){
+$sql="SELECT *,count(cod_lib) as conta FROM libros INNER JOIN prestamos using(cod_lib) group By cod_lib ORDER BY conta DESC limit 8";	
+return conex()->query($sql);
+}
+
+function conex(){
+    //return new mysqli("10.10.10.199","busters","1234","biblioteca");
+    return new mysqli("localhost","busters","1234","biblioteca");
+}
+
+function consulta($tabla){
+$sql="SELECT * FROM $tabla";
+return conex()->query($sql);
+}
+function insertar($tabla, $campo,$nom)
+{
+    $sql="INSERT INTO $tabla($campo) VALUES('$nom')";
+    return conex()->query($sql);
+}
+
+function verificar($campo,$tabla,$condi)
+{
+    $sql="SELECT $campo FROM $tabla WHERE $campo=$condi";
+    $ej=conex()->query($sql);
+    return $ej->fetch_array();
+}
+function estrella($valor)
+{
+$imprime="";
+$sql="SELECT cod_lib, avg(puntos_val) as suma FROM valoraciones WHERE cod_lib=$valor";
+$ejec= conex()->query($sql);
+$reg=$ejec->fetch_array();
+if($reg["suma"]!=NULL){
+  $valor=$reg["suma"];  
+  $eva=$valor - intval($valor);
+for($x=1;$x<=$valor;$x++)
+{
+    $imprime.='<i class="fa fa-star" style="color:#ff39ba;"></i>';
+}
+if($eva){ 
+    $imprime.='<i class="fa-solid fa-star-half-stroke" style="color:#ff39ba;"></i>';
+}
+}
+return $imprime;
+}
+
+function valorar($id,$codlib){
+    $sql="SELECT * FROM valoraciones WHERE cod_val='$id' AND act_val='0'";
+    $ver=conex()->query($sql);
+    if($ver->fetch_array())
+    {
+$a="comentar $codlib";
+
+    }
+    else
+    {
+$a= "link fue usado";
+    }
+return $a;
+
+
+
+}
+function pinta()
+{
+echo "";
+
+
+
+}
+
+?>
